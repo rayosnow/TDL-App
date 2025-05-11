0a3ce4a5fee01c23c04d59a5585b0eb871ca34e9
rayosnow <arayo1523@gmail.com>
2025-05-11 16:45:49 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,40 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('tareas-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/service-worker.js',
-        '/icon-192.png',
-        '/icon-512.png'
-      ]);
-    })
+// service-worker.js
+
+const CACHE_NAME = 'tdl-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Instalar y cachear
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+// Activar y limpiar versiones viejas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(
+        names.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      )
+    )
   );
 });
+
+// Interceptar peticiones
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
+
